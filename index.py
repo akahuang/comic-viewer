d047d94016e0da3d59dc1025ac15d224210c95f1
Chih-Yu Huang <flarehunter@storysensecomputing.com>
2014-01-26 15:30:58 +0800

    Add SearchBarForm class (TEMP)

--- index.py
+++ index.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 
+import sys
 from flask import Flask
 from flask import render_template
+from Forms import SearchBarForm
 
 app = Flask(__name__)
+app.debug = True
+app.secret_key = 'lala development key'
 
 @app.route('/')
-@app.route('/<name>/')
-def index(name=None):
-    return render_template('index.html', name=name)
+def index():
+    form = SearchBarForm()
+    print 'TEST: ', form.query.label
+    return render_template('index.html', form=form)
+
+def main(argv=sys.argv[:]):
+    app.run()
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
+
